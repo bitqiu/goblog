fee25f22a2afd8d12ab375c1fa7bf0b919e9b740
bitqiu <i@bitqiu.cc>
2020-11-17 17:56:26 +0800

    连接数据库

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"github.com/go-sql-driver/mysql"
 	"html/template"
+	"log"
 	"net/http"
 	"net/url"
 	"strings"
+	"time"
 	"unicode/utf8"
 
 	"github.com/gorilla/mux"
@@ -15,6 +17,40 @@ import (
 )
 
 var router = mux.NewRouter()
+var db *sql.DB
+
+func initDB() {
+	var err error
+	config := mysql.Config{
+		User:  		"root",
+		Passwd: 	"root",
+		Addr: 		"127.0.0.1:3306",
+		Net: 		"tcp",
+		DBName:  	"goblog",
+		AllowNativePasswords: true,
+	}
+
+	// 准备数据库连接池
+	db, err = sql.Open("mysql", config.FormatDSN())
+	checkError(err)
+
+	// 设置最大连接数
+	db.SetMaxOpenConns(25)
+	// 设置最大空闲连接数
+	db.SetMaxIdleConns(25)
+	// 设置每个链接的过期时间
+	db.SetConnMaxLifetime(5 * time.Minute)
+
+	// 尝试连接，失败会报错
+	err = db.Ping()
+	checkError(err)
+}
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func homeHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "<h1>Hello, 欢迎来到 goblog！</h1>")
@@ -133,11 +169,8 @@ func removeTrailingSlash(next http.Handler) http.Handler {
 	})
 }
 
-func init() {
-	sql.Register("mysql", &mysql.MySQLDriver{})
-}
-
 func main() {
+	initDB()
 
 	router.HandleFunc("/", homeHandler).Methods("GET").Name("home")
 	router.HandleFunc("/about", aboutHandler).Methods("GET").Name("about")
