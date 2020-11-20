380eb538f621c63901cc84a4792ee4ee7bcc656a
bitqiu <i@bitqiu.cc>
2020-11-20 11:12:32 +0800

    新建 Article 模型

--- app/http/controllers/articles_controller.go
+++ app/http/controllers/articles_controller.go
@@ -1,11 +1,13 @@
 package controllers
 
 import (
-	"database/sql"
 	"fmt"
+	"goblog/app/models/article"
 	"goblog/pkg/logger"
 	"goblog/pkg/route"
 	"goblog/pkg/types"
+	"gorm.io/gorm"
+	"html/template"
 	"net/http"
 )
 
@@ -21,10 +23,11 @@ func (*ArticlesController) Show(w http.ResponseWriter, r *http.Request) {
 	id := route.GetRouteVariable("id", r)
 
 	// 2. 读取对应的文章数据
-	article, err := getArticleByID(id)
+	article, err := article.Get(id)
+
 
 	if err != nil {
-		if err == sql.ErrNoRows {
+		if err == gorm.ErrRecordNotFound {
 			w.WriteHeader(http.StatusNotFound)
 			fmt.Fprint(w, "404 文章未找到")
 		} else {
