6b91f34d7a24abbff236ed9d802f748f1951ea7c
bitqiu <i@bitqiu.cc>
2020-11-23 11:19:09 +0800

    注册表单验证

--- app/http/controllers/auth_controller.go
+++ app/http/controllers/auth_controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"goblog/app/models/user"
+	"goblog/app/requests"
 	"goblog/pkg/view"
 	"net/http"
 )
@@ -16,26 +17,41 @@ func (*AuthController) Register(w http.ResponseWriter, r *http.Request) {
 	view.RenderSimple(w, view.D{}, "auth.register")
 }
 
+type userForm struct {
+	Name            string `valid:"name"`
+	Email           string `valid:"email"`
+	Password        string `valid:"password"`
+	PasswordComfirm string `valid:"password_comfirm"`
+}
+
 // DoRegister 处理注册逻辑
 func (*AuthController) DoRegister(w http.ResponseWriter, r *http.Request) {
 	// 0. 初始化变量
-	name := r.PostFormValue("name")
-	email := r.PostFormValue("email")
-	password := r.PostFormValue("password")
-
-	// 1. 表单验证
-	// 2. 验证通过 —— 入库，并跳转到首页
 	_user := user.User{
-		Name:     name,
-		Email:    email,
-		Password: password,
+		Name:            r.PostFormValue("name"),
+		Email:           r.PostFormValue("email"),
+		Password:        r.PostFormValue("password"),
+		PasswordComfirm: r.PostFormValue("password_comfirm"),
 	}
-	_user.Create()
 
-	if _user.ID > 0 {
-		fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
+	// 2. 表单规则
+	errs := requests.ValidateRegistrationForm(_user)
+
+	if len(errs) > 0 {
+		// 3. 表单不通过 —— 重新显示表单
+		view.RenderSimple(w, view.D{
+			"Errors": errs,
+			"User":   _user,
+		}, "auth.register")
 	} else {
-		w.WriteHeader(http.StatusInternalServerError)
-		fmt.Fprint(w, "创建文章失败，请联系管理员")
+		_user.Create()
+
+		if _user.ID > 0 {
+			fmt.Fprint(w, "插入成功，ID 为"+_user.GetStringID())
+		} else {
+			w.WriteHeader(http.StatusInternalServerError)
+			fmt.Fprint(w, "创建文章失败，请联系管理员")
+		}
 	}
+
 }
