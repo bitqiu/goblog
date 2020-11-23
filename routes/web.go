fbda35bd3bd98d8e7f960b2b71a99f32cdd75087
bitqiu <i@bitqiu.cc>
2020-11-23 10:09:59 +0800

    注册页面

--- routes/web.go
+++ routes/web.go
@@ -34,6 +34,10 @@ func RegisterWebRoutes(r *mux.Router) {
 	r.PathPrefix("/css/").Handler(http.FileServer(http.Dir("./public")))
 	r.PathPrefix("/js/").Handler(http.FileServer(http.Dir("./public")))
 
-	//r.Use(middlewares.ForceHTML)
+	// 用户认证
+	auc := new(controllers.AuthController)
+	r.HandleFunc("/auth/register", auc.Register).Methods("GET").Name("auth.register")
+	r.HandleFunc("/auth/do-register", auc.DoRegister).Methods("POST").Name("auth.doregister")
+
 
 }
